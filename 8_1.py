e61c588bbdaed554a6833081373d19f27c1bb803
megaman2020 <amegahed94@gmail.com>
2021-01-13 15:32:35 +0200

    Update 8_1.py

--- 8_1.py
+++ 8_1.py
@@ -4,4 +4,29 @@
 # steps at a time. Implement a method to count how many possible ways the child can run up the
 # stairs.
 
+# recursive solution
 
+def combs(n):
+    if n == 0 or n == 1 or n == 2:
+        return n
+    if n == 3:
+        return 4
+    return combs(n-1) + combs(n-2) + combs(n-3)
+
+# memoization/dynamic_programming solution
+
+def helper(n, mem):
+    if n == 0 or n == 1 or n == 2:
+        return n
+    if n == 3:
+        return 4
+    
+    if mem[n] is None:
+        mem[n] = combs(n-1) + combs(n-2) + combs(n-3)
+    return mem[n]
+    
+def combs_dp(n):
+    return helper(n, [None for i in range(n+1)])
+
+# print(combs_dp(50))
+# print(combs(50))
