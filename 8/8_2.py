2f6a2ee544b35386a0172c3ea001c9e5ab70b29e
megaman2020 <amegahed94@gmail.com>
2021-01-14 14:37:54 +0200

    Update 8_2.py

--- 8/8_2.py
+++ 8/8_2.py
@@ -4,3 +4,5 @@
 # The robot can only move in two directions, right and down, but certain cells are "off limits" such that
 # the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to
 # the bottom right
+
+https://stackoverflow.com/questions/64131960/robot-in-a-grid-how-to-get-all-possible-paths
